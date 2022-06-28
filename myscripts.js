da1e47f9c82f97c1e0801a350081013cdff09b39
Daniel Reyes Tong <Dreyestong@gmail.com>
2022-06-28 03:27:17 -0700

    Initial comments of algrothim are made

---
+++ myscripts.js
@@ -0,0 +1,10 @@
+// Create a function called computerPlay
+//function has computerPlay randomly return "Rock" "Paper" "Scissors"
+
+//Create a function that takes the parameter playerSelection and computerSelection
+//Return a string that declares teh winner of the round like "You Lose! Paper beats Rock"
+
+//write a function called game()
+//put playRound function inside of thius one to play a 5 round game, that keeps score and reports a winner at the end.
+
+console.log("Hello")
